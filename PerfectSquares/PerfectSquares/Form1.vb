315b05d9a0a3e4240e5587f242f19876669f8400
idsc3101 <idsc3101@gmail.com>
2013-10-09 17:12:03 -0500

    Finished in-class exercises

--- PerfectSquares/PerfectSquares/Form1.vb
+++ PerfectSquares/PerfectSquares/Form1.vb
@@ -3,7 +3,14 @@
     Private Sub btnDisplay_Click(sender As System.Object, e As System.EventArgs) Handles btnDisplay.Click
 
         lstResult.Items.Clear()
-        lstResult.Items.Add("Not implemented yet")
+
+        Dim i As Integer = 0
+
+        Do
+            i += 1
+            lstResult.Items.Add(CStr(i ^ 2))
+
+        Loop Until i = 10
 
     End Sub
 End Class
--- PopulationSim/PopulationSim/Form1.vb
+++ PopulationSim/PopulationSim/Form1.vb
@@ -4,7 +4,15 @@ Public Class Form1
 
     Private Sub btnPopulation_Click(sender As System.Object, e As System.EventArgs) Handles btnPopulation.Click
 
-        MessageBox.Show("Not implemented yet", "Error")
+        Dim curPop As Double = 7000
+        Dim curYear As Integer = 2012
+
+        Do While curPop > 6
+            curPop /= 2
+            curYear -= 50
+        Loop
+
+        MessageBox.Show("The year would be " & curYear, "" & curYear)
 
     End Sub
 End Class
